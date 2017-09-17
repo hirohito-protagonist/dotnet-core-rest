de6ad88faddfac2384e7f03bfe5ffa87ef00030f
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2017-09-17 16:02:29 +0200

    feat: add age calculation

---
+++ Helpers/DateTimeOffsetExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace dotnet_core_rest.Helpers
+{
+    public static class DateTimeOffsetExtensions
+    {
+        public static int GetCurrentAge(this DateTimeOffset dateTimeOffset)
+        {
+            var currentDate = DateTime.UtcNow;
+            int age = currentDate.Year - dateTimeOffset.Year;
+
+            if (currentDate < dateTimeOffset.AddYears(age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using dotnet_core_rest.Services;
+using dotnet_core_rest.Helpers;
 using AutoMapper;
 
 namespace dotnet_core_rest
@@ -42,6 +43,9 @@ namespace dotnet_core_rest
                 cfg.CreateMap<Entities.Author, Models.AuthorDto>()
                     .ForMember(dest => dest.Name, opt => opt.MapFrom(src =>
                         $"{src.FirstName} {src.LastName}"
+                    ))
+                    .ForMember(dest => dest.Age, opt => opt.MapFrom(src =>
+                        src.DateOfBirth.GetCurrentAge()
                     ));    
             });
 
