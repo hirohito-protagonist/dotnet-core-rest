26a1ea95f45107dd0b1902d652ca5dc64449b9ea
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2017-10-26 20:15:02 +0200

    test: add more integration test for authors endpoint

--- test/BooksLibrary/Tests/Integration/AuthorsTest.cs
+++ test/BooksLibrary/Tests/Integration/AuthorsTest.cs
@@ -16,18 +16,20 @@ namespace BooksLibrary.Tests.Integration
     public class AuthorsIntegrationTest : IntegrationTestsBase<Startup>
     {
 
-        [Fact]
-        public async void TestGetAuthors()
+        private void AddRequestIpLimitHeaders(HttpRequestMessage request)
         {
-
-            // Arrange
             var clientId = "cl-key-b";
             var ip = "::1";
 
-            // Act
-            var request = new HttpRequestMessage(HttpMethod.Get, "api/authors");
             request.Headers.Add("X-ClientId", clientId);
             request.Headers.Add("X-Real-IP", ip);
+        }
+
+        [Fact(DisplayName = "it should return collection of authors")]
+        public async void TestGetAuthors()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/authors");
+            this.AddRequestIpLimitHeaders(request);
 
             var response = await this.Client.SendAsync(request);
 
@@ -36,8 +38,35 @@ namespace BooksLibrary.Tests.Integration
             string raw = await response.Content.ReadAsStringAsync();
             List<AuthorDto> outputModel = JsonConvert.DeserializeObject<List<AuthorDto>>(raw);
 
-            // Assert
             Assert.Equal(6, outputModel.Count);
         }
+
+        [Fact(DisplayName = "it should return 404 not found status code when author could not be find by id")]
+        public async void TestGetAuthorWithInvalidId()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/authors/123test");
+            this.AddRequestIpLimitHeaders(request);
+
+            var response = await this.Client.SendAsync(request);
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact(DisplayName = "it should create author and return 201 status code on success")]
+        public async void TestAuthorCreation()
+        {
+            
+            var content = new StringContent(JsonConvert.SerializeObject(new AuthorCreationDto()),
+                                    Encoding.UTF8, "application/json");
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "api/authors");
+            request.Content = content;
+            this.AddRequestIpLimitHeaders(request);
+
+            var response = await this.Client.SendAsync(request);
+
+            Assert.Equal(201, (int)response.StatusCode);
+        }
+
     }
 }
