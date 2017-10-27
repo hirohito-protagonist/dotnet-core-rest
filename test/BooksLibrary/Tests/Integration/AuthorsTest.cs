6625627b47195ffde0e58c6ecd8dec3aa2ec1b57
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2017-10-27 20:56:01 +0200

    test: finish author integration test

--- test/BooksLibrary/Tests/Integration/AuthorsTest.cs
+++ test/BooksLibrary/Tests/Integration/AuthorsTest.cs
@@ -25,6 +25,22 @@ namespace BooksLibrary.Tests.Integration
             request.Headers.Add("X-Real-IP", ip);
         }
 
+        private async Task<AuthorDto> CreateDummyAuthor()
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(new AuthorCreationDto()),
+                                    Encoding.UTF8, "application/json");
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "api/authors");
+            request.Content = content;
+            this.AddRequestIpLimitHeaders(request);
+
+            var response = await this.Client.SendAsync(request);
+
+            string rawAuthor = await response.Content.ReadAsStringAsync();
+            
+            return JsonConvert.DeserializeObject<AuthorDto>(rawAuthor);
+        }
+
         [Fact(DisplayName = "it should return collection of authors")]
         public async void TestGetAuthors()
         {
@@ -52,6 +68,45 @@ namespace BooksLibrary.Tests.Integration
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         }
 
+        [Fact(DisplayName = "it should find author for specific id")]
+        public async void TestGetAuthorWithValidId()
+        {
+            AuthorDto author = await CreateDummyAuthor();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/authors/" + author.Id);
+            this.AddRequestIpLimitHeaders(request);
+
+            var response = await this.Client.SendAsync(request);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+
+       [Fact(DisplayName = "it should delete author for specific id")]
+        public async void TestDeleteAuthorWithValidId()
+        {
+            AuthorDto author = await CreateDummyAuthor();
+
+            var request = new HttpRequestMessage(HttpMethod.Delete, "api/authors/" + author.Id);
+            this.AddRequestIpLimitHeaders(request);
+
+            var response = await this.Client.SendAsync(request);
+
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+        }
+
+       [Fact(DisplayName = "it should return 404 status code on delete author action when can not find author for specific id")]
+        public async void TestDeleteAuthorWithInValidId()
+        {
+
+            var request = new HttpRequestMessage(HttpMethod.Delete, "api/authors/123test");
+            this.AddRequestIpLimitHeaders(request);
+
+            var response = await this.Client.SendAsync(request);
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
         [Fact(DisplayName = "it should create author and return 201 status code on success")]
         public async void TestAuthorCreation()
         {
@@ -67,6 +122,5 @@ namespace BooksLibrary.Tests.Integration
 
             Assert.Equal(201, (int)response.StatusCode);
         }
-
     }
 }
